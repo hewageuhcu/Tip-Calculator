4f0376ccf2cf4648e086e27fc8e1499617bb148a
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-27 21:32:45 +0900

    kotlin code added

--- app/src/main/java/np/com/test/tip_cal/MainActivity.kt
+++ app/src/main/java/np/com/test/tip_cal/MainActivity.kt
@@ -1,22 +1,44 @@
-//package np.com.test.tip_cal
-//
-//import android.os.Bundle
-//import androidx.activity.ComponentActivity
-//import androidx.activity.compose.setContent
-//import androidx.activity.enableEdgeToEdge
-//import androidx.compose.foundation.layout.fillMaxSize
-//import androidx.compose.foundation.layout.padding
-//import androidx.compose.material3.Scaffold
-//import androidx.compose.material3.Text
-//import androidx.compose.runtime.Composable
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.tooling.preview.Preview
-//import np.com.test.tip_cal.ui.theme.Tip_CalTheme
-//import android.view.View
-//import android.widget.Button
-//import android.widget.EditText
-//import android.widget.TextView
-//
-//class MainActivity : ComponentActivity() {
-//private EditText
-//}
+package np.com.test.tip_cal
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : ComponentActivity() {
+    private lateinit var etBillAmount: EditText
+    private lateinit var etTipPercent: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvTipResult: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tip_cal_main)
+
+        etBillAmount = findViewById(R.id.etBillAmount)
+        etTipPercent = findViewById(R.id.etTipPercent)
+        btnCalculate = findViewById(R.id.btnCalculate)
+        tvTipResult = findViewById(R.id.tvTipResult)
+
+        btnCalculate.setOnClickListener {
+            calculateTip()
+        }
+    }
+
+    private fun calculateTip() {
+        val billAmountStr = etBillAmount.text.toString()
+        val tipPercentStr = etTipPercent.text.toString()
+
+        if (billAmountStr.isNotEmpty() && tipPercentStr.isNotEmpty()) {
+            val billAmount = billAmountStr.toDouble()
+            val tipPercent = tipPercentStr.toDouble()
+            val tipAmount = (billAmount * tipPercent) / 100
+
+            tvTipResult.text = "Tip Amount: $${"%.2f".format(tipAmount)}"
+        } else {
+            tvTipResult.text = "Please enter both values"
+        }
+    }
+}
