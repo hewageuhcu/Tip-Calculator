ba1552e19414a724ee393314608c6e0bcd10af6a
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-27 21:03:49 +0900

    Java code added

---
+++ app/src/main/java/np/com/test/tip_cal/MainActivity.kt
@@ -0,0 +1,22 @@
+//package np.com.test.tip_cal
+//
+//import android.os.Bundle
+//import androidx.activity.ComponentActivity
+//import androidx.activity.compose.setContent
+//import androidx.activity.enableEdgeToEdge
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.material3.Scaffold
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.tooling.preview.Preview
+//import np.com.test.tip_cal.ui.theme.Tip_CalTheme
+//import android.view.View
+//import android.widget.Button
+//import android.widget.EditText
+//import android.widget.TextView
+//
+//class MainActivity : ComponentActivity() {
+//private EditText
+//}
